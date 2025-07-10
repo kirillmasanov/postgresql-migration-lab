1db9a3b008782a32c64c153feeb85bb435f84982
Kirill Masanov <kimasanov@yandex.ru>
2025-07-10 13:38:22 +0300

    fix

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,20 @@
-# output "source_db_public_ip" {
-#   description = "Public IP address of the source database VM"
-#   value       = yandex_compute_instance.source_db.network_interface.0.nat_ip_address
-# }
-
 output "source_db_ssh_command" {
   description = "SSH command to connect to source database VM"
   value       = "ssh ${var.vm_user}@${yandex_compute_instance.source_db.network_interface.0.nat_ip_address}"
 }
 
-# output "target_db_host" {
-#   description = "Hostname of the target Managed PostgreSQL cluster"
-#   value       = yandex_mdb_postgresql_cluster.target_db.host[0].fqdn
-# }
+output "target_db_host" {
+  description = "Hostname of the target Managed PostgreSQL cluster"
+  value       = yandex_mdb_postgresql_cluster.target_db.host[0].fqdn
+}
 
 # output "target_db_port" {
 #   description = "Port of the target Managed PostgreSQL cluster"
 #   value       = yandex_mdb_postgresql_cluster.target_db.host[0]
 # }
 
-# output "target_db_connection_string" {
-#   description = "Connection string for target database"
-#   value       = "postgresql://admin:${var.pg_password}@${yandex_mdb_postgresql_cluster.target_db.host[0].fqdn}:6432/migration_db"
-#   sensitive   = true
-# }
+output "target_db_connection_string" {
+  description = "Connection string for target database"
+  value       = "postgresql://admin:${var.pg_password}@${yandex_mdb_postgresql_cluster.target_db.host[0].fqdn}:6432/migration_db"
+  sensitive   = true
+}
