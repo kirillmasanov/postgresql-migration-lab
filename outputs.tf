0c62bd2d083bc7a2c5c6f532eb85e1327f54bfbe
Kirill Masanov <kimasanov@yandex.ru>
2025-07-15 22:49:34 +0300

    add target_db_host output

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,7 @@ output "source_db_ssh_command" {
   value       = "ssh ${var.vm_user}@${yandex_compute_instance.source_db.network_interface.0.nat_ip_address}"
 }
 
+output "target_db_host" {
+  description = "Hostname of the target Managed PostgreSQL cluster"
+  value       = yandex_mdb_postgresql_cluster.target_db.host[0].fqdn
+}
